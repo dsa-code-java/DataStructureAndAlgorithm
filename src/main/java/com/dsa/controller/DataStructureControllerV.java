e9d78f842e25939b65e2f348282ff033caac7a06
Vedant Jadhav <veerujadhav879@gmail.com>
2025-11-30 00:03:11 +0530

    Binary search code

--- src/main/java/com/dsa/controller/DataStructureControllerV.java
+++ src/main/java/com/dsa/controller/DataStructureControllerV.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -53,6 +54,16 @@ public class DataStructureControllerV {
 				break;
 		}
 		logger.info("Exit from sortingAlgo");	
+		return commonResponse;
+	}
+
+	@PostMapping("/binarySearch")
+	public CommonResponse binarySearch(@RequestParam int[] arr) throws Exception {
+		logger.info("Entry in binarySearch");
+		
+		CommonResponse commonResponse = new CommonResponse(HttpStatus.OK.value(), DsaAlgoConstants.SUCCESS, dataStructureServiceV.ping());
+		
+		logger.info("Entry in binarySearch");
 		return commonResponse;
 	}
 }
